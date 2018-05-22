010e7e98af94f7ad7718c1db4cb5f1ed00efe5af
gislene lima <vanessa@fiberlink.net.br>
2018-05-22 17:28:47 -0300

    criada a sessao de Course-admin

--- simplemooc/courses/admin.py
+++ simplemooc/courses/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Course #importa models que vai querer cadastrar
+
+admin.site.register(Course)
--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class CourseManager (models.Manager):
+    #medodo para realizar busca por nome ou slug
+    def search(self, query):
+        return self.get_queryset().filter(
+            models.Q(name__icontains=query) | #ou
+            models.Q(description__icontains=query)#models.Q é uma função já inclusa
+            
+        )
+
 
 class Course(models.Model):
     
@@ -8,7 +17,8 @@ class Course(models.Model):
     slug = models.SlugField('Atalho')
     description = models.TextField('Descrição', blank = True)
     start_date = models.DateField('Date de Início', null = True, blank = True)
-    image = models.ImageField(upload_to='couses/images', verbose_name='Imagem')
+    image = models.ImageField(upload_to='couses/images', verbose_name='Imagem', null = True, blank = True)
     create_at = models.DateTimeField('Criado em',auto_now_add = True)
     update_at = models.DateTimeField('Atualizado em', auto_now = True)
 
+    objects = CourseManager()
