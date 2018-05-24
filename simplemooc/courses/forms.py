70847d9e7058a7a61419399e275302eda7a51b39
gislene lima <vanessa@fiberlink.net.br>
2018-05-24 18:00:33 -0300

    envio de email funcionando

--- simplemooc/courses/forms.py
+++ simplemooc/courses/forms.py
@@ -1,7 +1,31 @@
 from django import forms
+from django.core.mail import send_mail
+from django.conf import settings
 
 class ContactCourse(forms.Form):
 
     name = forms.CharField(label = 'Nome', max_length=100)
     email = forms.EmailField(label = 'E-mail')
     message = forms.CharField(label = 'Mesagem/Dúvida', widget = forms.Textarea)
+
+
+#regras de negicio sempre deixar por aqui
+
+    def send_mail(self, course):
+        subject = '[%s] Contato' % course 
+        message = 'Nome: %(name)s; E-mail:%(email)s; %(message)s'
+        context = {
+            'name': self.cleaned_data['name'],
+            'email': self.cleaned_data['email'],
+            'message': self.cleaned_data['message'],
+            
+        }
+
+        template_name = 'courses/contact_email.html'
+        send_mail(
+            subject, message, settings.DEFAULT_FROM_EMAIL, 
+        [settings.CONTACT_EMAIL]
+        )
+
+
+
--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -12,7 +12,18 @@ def index(request):
 
 def details(request, slug): #descrição do curso
     course = get_object_or_404(Course, slug=slug)
-    context = {'course':course,'form': ContactCourse()}
+    context = {}
+    if request.method =='POST':
+        form = ContactCourse(request.POST)
+        if form.is_valid():   #verifica validação
+            context['is_valid'] = True
+            form.send_mail(course) 
+            form = ContactCourse()
+            
+    else: 
+        form = ContactCourse()
+    context['form'] = form 
+    context['course']=course
     template_name = 'courses/details.html'
     return render (request, template_name, context)
     
