ede7775f42c4e0b95ea8c1bafa5e5f4ea08ea9f6
gislene lima <vanessa@fiberlink.net.br>
2018-05-25 16:04:07 -0300

    cadastro de usuario com autenticação estao funcionando

--- simplemooc/accounts/views.py
+++ simplemooc/accounts/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.conf import settings
+from django.shortcuts import redirect
 
 # Create your views here.
 
 def register(request):
     template_name = 'accounts/register.html'
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(settings.LOGIN_URL) #faz redirecionamento para a page de login
+            
+    else:
+        form = UserCreationForm()
+              
     context = {
-        'form': UserCreationForm()
+        'form':form
     }
     return render(request, template_name, context)
