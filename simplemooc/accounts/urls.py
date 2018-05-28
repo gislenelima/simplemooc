972a9f03ae40a2ae63c909d369edff773eccd540
gislene lima <vanessa@fiberlink.net.br>
2018-05-28 16:59:14 -0300

    Funcionalidade de login e logout 'ok'!

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path,include
 from django.conf.urls import url
 from simplemooc.accounts import views
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login,logout
 
 
 urlpatterns = [
     path('entrar/',login, {'template_name':'accounts/login.html'}, name = 'login'),
     path('cadastre-se/',views.register, name = 'register'),
+    path('sair/',logout, {'next_page':'home'}, name = 'logout'),#repassado o link nao a template + o if no home
 ]
 
--- simplemooc/accounts/views.py
+++ simplemooc/accounts/views.py
@@ -2,19 +2,28 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
 from django.shortcuts import redirect
+from .forms import RegisterForm
+from django.contrib.auth import authenticate, login
+
+
 
 # Create your views here.
 
 def register(request):
     template_name = 'accounts/register.html'
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)#cria novos usuarios
         if form.is_valid():
-            form.save()
+            user = form.save()
+            user = authenticate( #faz autenticação depois do cadastro| usuario já fica logado
+                username = user.username, password = form.cleaned_data['password1']
+            )
+            login (request, user) #colocará o usuario na sessao
+            
             return redirect(settings.LOGIN_URL) #faz redirecionamento para a page de login
             
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
               
     context = {
         'form':form
