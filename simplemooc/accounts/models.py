ed8a0e65dfeec4af24092e73a4eb525cc1442727
gislene lima <vanessa@fiberlink.net.br>
2018-05-29 18:04:28 -0300

    na 48

--- simplemooc/accounts/models.py
+++ simplemooc/accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin, UserManager)
 from django.core import validators
-
+from django.conf import settings
 
 # Create your models here.
 
@@ -32,4 +32,21 @@ class User(AbstractBaseUser, PermissionsMixin):
     class Meta: 
         verbose_name = 'Usuário'
         verbose_name_plural = 'Usuários'
+
+
+    class PasswordReset(models.Model):  #um usuario pode ter um ou mais passwordReset
+    
+        user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name = 'Usuário',on_delete=models.CASCADE)
+        key = models.CharField('Chave', max_length = 100, unique = True)
+        created_at = models.DateTimeField('Criado em ', auto_now_add = True)
+        confirmed = models.BooleanField('Confirmado?', default = False, blank = True )
+
+        def __str__(self):
+            return '{0} em {1}'.format(self.user, self.created_at)
+        
+        class Meta:
+            verbose_name = 'Nova Senha'
+            verbose_name_plural = 'Novas Senhas'
+            ordering = ['-created_at']
+
         
